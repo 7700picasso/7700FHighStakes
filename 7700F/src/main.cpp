140e138d7f67dba13f43b59650d27048c4d1fabc
Areophix <7700@rollingrobots.com>
2025-03-12 16:00:22 -0700

    Update main.cpp

--- 7700F/src/main.cpp
+++ 7700F/src/main.cpp
@@ -25,7 +25,7 @@ inertial Gyro (PORT15);
 float pi = 3.14;
 float dia = 3.25;
 float gearRatio = 1.6;
-float armPositions[] = {0.0, 75, 545};
+float armPositions[] = {0.0, 60, 545};
 int currentPositionindex = 0;
 float target = 0;
 
